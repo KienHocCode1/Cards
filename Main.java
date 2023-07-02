6f1dafdc407c9774ad9623eab65b612500958567
KienHocCode1 <136813027+KienHocCode1@users.noreply.github.com>
2023-07-02 19:15:42 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -10,30 +10,32 @@ public class Main {
         System.out.print("How many suit: ");
         int suit = input.nextInt();
 
-        DeckOfCards a = new DeckOfCards(rank, suit);
-        System.out.print(a);
-
-
+        DeckOfCards deck = new DeckOfCards(rank, suit);
+        System.out.print(deck);
+        System.out.println(" ");
+        deck.print();
+        deck.getTotalValue();
         boolean Flag = true;
         while (Flag) {
             System.out.println("");
             System.out.println("1=shuffle, 2=deal 1 hand, 3=deal 100000 times, 4=quit");
+
             int choice = input.nextInt();
             if (choice == 1) {
-                a.shuffle();
-                System.out.println("");
-                System.out.println(a);
+                deck.shuffle();
                 System.out.println("Cards are shuffled.");
+                System.out.println("");
+                System.out.println(deck);
 
             } else if (choice == 2) {
                 System.out.println("How many cards? ");
                 int deal1HandCards = input.nextInt();
-
+                deck.dealTopCards(deal1HandCards);
 
             } else if (choice == 3) {
                 System.out.println("How many cards? ");
                 int shuffle100000 = input.nextInt();
-                a.histogram();
+                deck.histogram(shuffle100000);
 
             }else {
                 System.out.println("Bye!");
